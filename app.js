947097c6520f7bd7d77126616251e4f517799d8e
juplmn777 <julien.plumyoen@gmail.com>
2021-03-18 11:41:36 +0100

    javascript for closing mobile-navbar when clinking on nav-links done

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const menu = document.querySelector('.nav-list');
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+/* ====== closing navbar mobile view ======*/
+
+const allLinks = document.querySelectorAll('.nav-item');
+
+allLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+});
